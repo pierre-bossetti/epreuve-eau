f369e1f630a3ce42e5bc8f340376373688e364b7
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-02 20:19:27 +0200

    Combinaison 2 nombres

---
+++ eau01.js
@@ -0,0 +1,20 @@
+let combinaison1 = 00;
+let combinaison2 = 01;
+let chiffre = 1;
+let stringNumbers = '';
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let k = 0; k < 10; k++) {
+      for (let l = 0; l < 10; l++) {
+        if (k == 9 && l == 9) {
+          stringNumbers += `${i}${j} ${k}${l}`;
+        } else {
+          stringNumbers += `${i}${j} ${k}${l}, `;
+        }
+      }
+    }
+  }
+}
+
+console.log(stringNumbers);
