dda88a952c7e70dc28b49a08938b797fc53f3fb3
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-02 20:29:15 +0200

    Combinaison deux chiffres correction

--- eau01.js
+++ eau01.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
     for (let k = 0; k < 10; k++) {
       for (let l = 0; l < 10; l++) {
-        if (k == 9 && l == 9) {
+        if (i == 9 && j == 9 && k == 9 && l == 9) {
           stringNumbers += `${i}${j} ${k}${l}`;
         } else {
           stringNumbers += `${i}${j} ${k}${l}, `;
