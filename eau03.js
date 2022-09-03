71bd29e23d26d4329002dad94f3c1960787e6d6b
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-03 11:20:01 +0200

    Fibonacci

--- eau03.js
+++ eau03.js
@@ -1,7 +1,9 @@
 const inputNumber = process.argv[2];
 
 function fibonacci(n) {
-  return n < 1
+  return n < 0
+    ? -1
+    : n < 1
     ? 0
     : n <= 2
     ? 1
@@ -10,4 +12,8 @@ function fibonacci(n) {
     : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(inputNumber));
+if (inputNumber > 0) {
+  console.log(fibonacci(inputNumber));
+} else {
+  console.log('erreur, saisir un chiffre svp.');
+}
