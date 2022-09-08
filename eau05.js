e58ffdc7c0218ff90117ce738c76742ba22925d5
Pierre <pierrebossetti_348@hotmail.fr>
2022-09-08 09:30:56 +0200

    Rendu final

--- eau05.js
+++ eau05.js
@@ -2,8 +2,6 @@
 const string1 = process.argv[2];
 const string2 = process.argv[3];
 
-const stringIncludes = function (string1, string2) {};
-
 if (typeof string1 == 'undefined' || typeof string2 == 'undefined') {
   console.log('error.');
 } else if (string1.includes(string2) || string2.includes(string1)) {
@@ -11,17 +9,3 @@ if (typeof string1 == 'undefined' || typeof string2 == 'undefined') {
 } else {
   console.log('false');
 }
-
-/*
-let similar = false;
-let i = 0;
-let j = 0;
-while (string1[i] != string2[j]) {
-  // similar = false;
-  for (let j = i + j; string1[j] == string2[j]; j++) {
-    similar = true;
-  }
-  i++;
-}
-console.log(similar);
-*/
